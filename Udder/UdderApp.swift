af21e877b297342a644ad30b88e40c308dd7cfe3
Scott Strand <sstrand@gmail.com>
2023-05-24 13:34:42 -0700

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Scott Strand on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
