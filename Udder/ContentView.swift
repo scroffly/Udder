d8238f23236672c2f38ca4fe0f14ea1a8a525300
Scott Strand <sstrand@gmail.com>
2023-05-24 13:41:43 -0700

    Change view controller

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Udder app!")
+            Text("Milk time!")
                 
         }
         .padding()
